28ac2ca2bc146181fded2f02dd2950a6b6626b0c
yufan2hao <lovefancy2009@gmail.com>
2017-02-28 00:18:18 +0800

    Update

---
+++ JsonLib.py
@@ -0,0 +1,16 @@
+from HttpLibrary import HTTP
+
+
+class JsonLib(object):
+
+    def load_json_data_from_file(self, filepath):
+        """ Don't forget to document the keyword!!!
+        """
+        json_string = open(filepath).read()
+        HTTP().should_be_valid_json(json_string)
+        #print type(json_string)
+        return json_string
+
+
+
+#JsonLib().load_json_data_from_file('./cookie.json')
