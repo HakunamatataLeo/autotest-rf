289344306d27cce9f2657078128dfb4bb2257808
yufan2hao <lovefancy2009@gmail.com>
2017-03-20 01:19:47 +0800

    update

--- auto-postman/case_template.py
+++ auto-postman/case_template.py
@@ -7,11 +7,12 @@ def _POST(self):
     body = self.param['body']
     expect = self.param['expect']
 
-    r = requests.post(url, headers=headers, json=body)
+    r = requests.post(url, headers=headers, data=body)
     assert r.ok
+    print body
     resp_data = r.json()
     for k, v in expect.iteritems():
-        assert resp_data[k] == v
+        assert resp_data[k] == v, "%s" % resp_data
 
 
 def _GET(self):
@@ -20,7 +21,7 @@ def _GET(self):
     body = self.param['body']
     expect = self.param['expect']
 
-    r = requests.get(url, headers=headers, json=body)
+    r = requests.get(url, headers=headers, data=body)
     assert r.ok
     resp_data = r.json()
     for k, v in expect.iteritems():
--- auto-postman/t.py
+++ auto-postman/t.py
@@ -6,15 +6,18 @@ import case_template
 from HTMLTestRunner import HTMLTestRunner
 
 records = {
-    '1': [
-        ('danmu', 'POST', 'http://api.live.bilibili.com/msg/send', {}, {}, {}),
-        ('gift', 'GET', 'http://api.live.bilibili.com/gift', {}, {}, {}),
-    ],
-
     '2': [
-        ('danmu', 'POST', 'http://api.live.bilibili.com/msg/send', {}, {}, {}),
-        ('gift', 'GET', 'http://api.live.bilibili.com/gift', {}, {}, {}),
+        ('x', 'POST', 'http://xx.com/msg/send',
+        {'X-Requested-With': 'XMLHttpRequest', 'COOKIE': 'xxx'},
+        {"color":16777215,"fontsize":25,"mode":1,"msg":"xx","rnd":1481192791,"roomid":x},
+        {"code":0}),
     ],
+    '1': [        
+        ('x', 'POST', 'http://xx.com/msg/send',
+        {'X-Requested-With': 'XMLHttpRequest', 'COOKIE': 'xxx'},
+        {"color":16777215,"fontsize":25,"mode":1,"msg":"xx","rnd":1481192791,"roomid":x},
+        {"code":0}),
+    ]
 }
 
 
@@ -34,11 +37,17 @@ def gen_test_suite():
             fn = getattr(case_template, '_'+case[1])
             fn.__doc__ = u'测试'
             setattr(scence_cls, fn_name, fn)
-            cases_name.append(fn_name)
+            cases_name.append([fn_name, case[2:]])
 
         # 添加测试用例到测试套件中
-        for case_name in cases_name:
+        for case_name, params in cases_name:
             t = scence_cls(case_name)
+            t.param = {
+                'url': params[0],
+                'headers': params[1],
+                'body': params[2],
+                'expect': params[3],
+            }
             testsuit.addTest(t)
 
     return testsuit
